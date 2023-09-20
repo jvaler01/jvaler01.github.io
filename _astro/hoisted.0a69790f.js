c6ae19706f1fb249ce12a396545941dfb49a6e08
Jorge Valero <jorgevm13@gmail.com>
2023-09-20 20:40:46 +0200

    first commit

---
+++ _astro/hoisted.0a69790f.js
@@ -0,0 +1,1 @@
+import"./hoisted.a23147bd.js";document.addEventListener("astro:page-load",()=>{const e=document.getElementById("home");e.addEventListener("click",()=>{e.style.display="none",document.getElementById("indexLoader").style.display="block",document.getElementById("containerLoader").classList.toggle("none"),setTimeout(()=>{document.getElementById("test")?.click()},4150)})},{once:!1});
